b8dcfaa91cd875311b032be5a5e4f11fc8c6b588
I-A11 <d.islam1983@gmail.com>
2021-09-14 09:38:48 +1000

    create space-tours react app

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import { FaRocket } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='nav-container'>
+        <FaRocket />
+        <h4>SpaceTours</h4>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
