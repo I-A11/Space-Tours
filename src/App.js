7947db87e4b7b7b7dc2c2d182cee22e3f1f9cd8b
I-A11 <d.islam1983@gmail.com>
2021-09-14 12:15:22 +1000

    finishing hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className='App'>
+    <>
       <Navbar />
-    </div>
+      <Hero />
+    </>
   );
 }
 
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,21 @@
+const Hero = () => {
+  return (
+    <>
+      <div className='hero-container'>
+        <h1>Discover The Space</h1>
+        <div className='hero-content'>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
+            cupiditate? Officia itaque nam iure ea amet. Natus suscipit
+            reiciendis, eveniet ex, molestias dicta accusantium distinctio
+            doloremque enim adipisci sequi asperiores dolor! Eum repudiandae
+            odit fugit. Quod quaerat optio dolorem numquam odit sint blanditiis
+            adipisci sunt animi.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
