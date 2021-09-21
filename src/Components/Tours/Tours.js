4f26b0955dd41ce403b7fe9450215a8f0992bf1a
I-A11 <d.islam1983@gmail.com>
2021-09-21 12:21:57 +1000

    fix tours section

--- src/Components/Tours/Tours.js
+++ src/Components/Tours/Tours.js
@@ -1,6 +1,31 @@
 import Tour from "./Tour";
 
 const Tours = ({ tours, removeTour, fetchTours }) => {
+  if (tours.length < 2) {
+    return (
+      <section className='tours-section'>
+        <div className='title'>
+          <h2>Our Tours</h2>
+          <div className='underline'></div>
+        </div>
+        <div
+          className='tours'
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr",
+            gridGap: "35px",
+            alignItems: "center",
+            maxWidth: "550px",
+            margin: "0 auto",
+          }}
+        >
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+          })}
+        </div>
+      </section>
+    );
+  }
   return (
     <section className='tours-section'>
       <div className='title'>
